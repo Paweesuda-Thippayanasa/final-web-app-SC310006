12da92de77494b4a2644e1fca059f5344c0ebf6e
Paweesuda-Thippayanasa <paweesuda.t@kkumail.com>
2025-02-15 02:04:12 +0700

    edit  config

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 console.log("App is being rendered");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/final-web-app-SC310006/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
