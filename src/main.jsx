25a67d4b64b545550d578c9e3f946b91fd91492c
Paweesuda-Thippayanasa <paweesuda.t@kkumail.com>
2025-02-15 02:23:27 +0700

    [update]:path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 console.log("App is being rendered");
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename='/final-web-app-SC310006/'>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+
+	// <BrowserRouter basename='/final-web-app-SC310006/'>  use when deploy
+	// <BrowserRouter> use when develop
+	<StrictMode>
+		{/* <BrowserRouter> */}
+			<BrowserRouter basename='/final-web-app-SC310006/'> 
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 );
