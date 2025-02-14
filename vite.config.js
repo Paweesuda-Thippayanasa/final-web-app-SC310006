7b846fc2625a352b56c718ab6f12468cc58972e0
Paweesuda-Thippayanasa <paweesuda.t@kkumail.com>
2025-02-15 01:57:29 +0700

    edit  config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-  base: process.env.NODE_ENV === 'development' ? '/' : '/final-web-app-SC310006', // ใช้ '/' ในการพัฒนาและ '/final-web-app-SC310006/' ในการ build
+  base: process.env.NODE_ENV === 'development' ? '/' : '/final-web-app-SC310006/', // ใช้ '/' ในการพัฒนาและ '/final-web-app-SC310006/' ในการ build
 })
