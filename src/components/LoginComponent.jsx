b11bedb8ffebd452b92ee74f675995948f29b08e
Paweesuda-Thippayanasa <paweesuda.t@kkumail.com>
2025-02-14 22:42:39 +0700

    [update]: config file to deploy on production and test on dev mode

---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,11 @@
+// Login page
+
+const LoginComponent = () => {
+	return (
+		<div>
+			<h2>Login</h2>
+		</div>
+	);
+};
+
+export default LoginComponent;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/final-web-app-SC310006",
+  plugins: [react(),  tailwindcss(),],
+  base: process.env.NODE_ENV === 'development' ? '/' : '/final-web-app-SC310006', // ใช้ '/' ในการพัฒนาและ '/final-web-app-SC310006/' ในการ build
 })
